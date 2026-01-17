fc0fda06c73d976c3af611ec85d9b54b014eec68
kellywsq03 <kellywsq03@gmail.com>
2026-01-17 22:02:08 +0800

    Add routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import TitleCard from "./components/TitleCard";
 import TravelInput from "./components/TravelInput";
 import TravelResults from "./components/TravelResults";
@@ -7,18 +8,25 @@ const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 function App() {
   return (
-    <APIProvider apiKey={GOOGLE_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
-      <div className="flex flex-col w-screen h-screen bg-white">
-        <div className="flex-none">
-          <TitleCard className="w-full" logo="planefella.png" />
+    <BrowserRouter>
+      <APIProvider apiKey={GOOGLE_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
+        <div className="flex flex-col w-screen h-screen bg-white">
+          <div className="flex-none">
+            <Link to ="/">
+              <TitleCard className="w-full" logo="planefella.png" />
+            </Link>
+          </div>
+          <div className="flex-1 pt-25">
+            <Routes>
+              <Route path="/" element={<TravelInput className="w-full h-full" />} />
+              <Route path="/about" element={<TravelResults className="w-full h-full"/>} />
+            </Routes>
+          </div>
         </div>
-        <div className="flex-1 pt-25">
-          <TravelInput className="w-full h-full" />
-          {/* <TravelResults className="w-full h-full"/> */}
-        </div>
-        
-      </div>
-    </APIProvider>
+      </APIProvider>
+
+      
+    </BrowserRouter>
   );
 }
 
